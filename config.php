6d275ff754686be84353bc63058f11df1b9dd001
Admiralteistvo <89741031+Admiralteistvo@users.noreply.github.com>
2022-01-15 22:01:18 +0300

    Add files via upload

--- config.php
+++ config.php
@@ -1,11 +1,12 @@
 <?php
     define('USER', 'root');
-    define('PASSWORD', '');
+    define('PASSWORD', 'root');
     define('HOST', 'localhost');
-    define('DATABASE', 'test');
+    define('DATABASE', 'authorization');
     define('www','http://localhost/PhoneBook/');
     try {
         $connection = new PDO("mysql:host=".HOST.";dbname=".DATABASE, USER, PASSWORD);
+
     } catch (PDOException $e) {
         exit("Error: " . $e->getMessage());
     }
--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+
 session_start();
-include('config.php');
+
+dirname ('http://domains/localhost/photobook/config.php');
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,50 +13,60 @@ include('config.php');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Вход</title>
+    <link href="css/style.css" rel="stylesheet">
 </head>
+
 
 <body>
     <form action="<?php echo www . "index.php" ?> " method="post">
-        <?php
-        echo '<h2>Вход</h2>';
-        echo 'Имя пользователя';
-        ?>
-        <br>
-        <input type='text' name='username' id='login' />
-        <br>
-        <?php
-        echo 'Пароль';
-        ?>
-        <br>
-        <input type='text' name='password' id='password' />
-        <br>
-        <input type="submit" name="login" value="Войти">
+        <header>
+            <div class = "title">PhoneBook</div>
+        </header>
+        <div class="main">
+            <h2>Вход</h2>
+            <?php
+            echo 'Имя пользователя';
+            ?>
+            <br>
+            <input type='text' name='username' id='login' />
+            <br>
+            <?php
+            echo 'Пароль';
+            ?>
+            <br>
+            <input type='text' name='password' id='password' />
+            <br>
+            <input class='submit' type="submit" name="login" value="Войти">
+        </div>
     </form>
     <form>
-        <?php
+<?php
+        $hash = '$2y$10$Vb.pry5vRGNrm6Y79UfBsun/RbXq2.XEGCOMpozrDwg.MNpfxvWHK';
         if (isset($_POST["username"])){echo $_POST['username'];}
         if (isset($_POST["password"])){echo $_POST['password'];}
         if (isset($_POST['login'])) {
-           echo "привет";
-            echo $username = $_POST['username'];
-            echo $password = $_POST['password'];
-            $query = $connection->prepare("SELECT * FROM users WHERE username=:username");
+            // echo $username = $_POST['username'];
+            // echo $password = $_POST['password'];
+            $query = $connection->prepare("SELECT * FROM `authorization` WHERE Login=:username");
             $query->bindParam("username", $username, PDO::PARAM_STR);
             $query->execute();
             $result = $query->fetch(PDO::FETCH_ASSOC);
+
+
             if (!$result) {
                 // if () {
 
                 echo '<p class="error">Неверные пароль или имя пользователя!</p>';
             } else {
                 if (password_verify($password, $result['password'])) {
-                    $_SESSION['user_id'] = $result['id'];
+                    $_SESSION['username'] = $result['id'];
                     echo '<p class="success">Поздравляем, вы прошли авторизацию!</p>';
                 } else {
                     echo '<p class="error"> Неверные пароль или имя пользователя!</p>';
                 }
             }
         }
+
         ?>
     </form>
 </body>
